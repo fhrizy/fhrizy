96e5fc5014450d6786cae2da970bf2ffa7fdaabb
fhrizy <mohamad.ficharrozy.te17@mhsw.pnj.ac.id>
2022-06-27 00:24:15 +0700

    test directory

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,27 +1,26 @@
-import  "./contact.scss";
-import {useState} from "react";
+import "./contact.scss";
+import { useState } from "react";
 
-export default function Contact(){
-
-    const [message, setMessage] = useState(false)
-    const handleSubmit = (e)=> {
-        e.preventDefault();
-        setMessage(true)
-    }
-    return (
-        <div className="contact" id="contact">
-            <div className="left">
-                <img src="assets/last.jpg" alt="" />
-            </div>
-            <div className="right">
-                <h2>contact.</h2>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Email"/>
-                    <textarea placeholder="Your Message"></textarea>
-                    <button type="submit">Send</button>
-                    {message && <span>Thanks, I'll reply ASAP</span>}
-                </form>
-            </div>
-        </div>
-    )
+export default function Contact() {
+  const [message, setMessage] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/last.jpg" alt="" />
+      </div>
+      <div className="right">
+        <h2>contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Your Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I'll reply ASAP</span>}
+        </form>
+      </div>
+    </div>
+  );
 }
